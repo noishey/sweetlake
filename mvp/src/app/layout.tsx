7c402c090e5181a6687175d1b44f1ac9bb11d665
Arjun Shenoy <arjunshenoy23@gmail.com>
2025-04-04 23:25:18 +0530

    feat(ui+auth): logo, dynamic-auth
    - Started logo creation setup
    - Integrated dynamic signup (currently causing page bugs)
    - Will debug in next commit

--- mvp/src/app/layout.tsx
+++ mvp/src/app/layout.tsx
@@ -1,20 +1,27 @@
 import './globals.css';
-import { Inter } from 'next/font/google';
-import Navbar from '@/components/ui/Navbar';
+import { Urbanist } from 'next/font/google';
+import Navbar from '@/components/ui/navbar';
+import DynamicProvider from '@/components/web3/dynamic-provider';
+import type { Metadata } from 'next';
 
-const inter = Inter({ subsets: ['latin'] });
+const urbanist = Urbanist({ subsets: ['latin'] });
 
-export const metadata = {
-  title: 'Sweetlake',
-  description: '---',
+export const metadata: Metadata = {
+  title: 'sweetlake',
+  description: 'A sleek modern experience.',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#0a0a0a] text-white`}>
-        <Navbar />
-        <main className="pt-16">{children}</main>
+      <body className={`${urbanist.className} bg-[#0a0a0a] text-white`}>
+        <DynamicProvider>
+          <main className="pt-16">{children}</main>
+        </DynamicProvider>
       </body>
     </html>
   );
